c582cad38e834ed8a28cef7447c92017e3387315
HemisCoding <plushytowel@gmail.com>
2023-05-17 21:46:35 +0300

    test8

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const knex = require('knex');
 const db = knex({
     client: 'pg',
     connection: {
-      connectionString: process.env.DATABASE_URL,
+      connectionString: 'postgres://mydb_z8yy_user:aGSUKasYb5O0PCpy5fFXj7UjSYKb3xzl@dpg-chh7s2u7avjd1v2p2mi0-a.oregon-postgres.render.com/mydb_z8yy',
       ssl: { rejectUnauthorized: false},
       host : 'dpg-chh7s2u7avjd1v2p2mi0-a.oregon-postgres.render.com',
       port : 5432,
