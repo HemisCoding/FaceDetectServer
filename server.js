4560d15b1f4ab70a5ccc09a8a8a28a59215b743d
HemisCoding <plushytowel@gmail.com>
2023-05-17 21:20:27 +0300

    5

--- server.js
+++ server.js
@@ -115,7 +115,7 @@ app.put('/image', (req, res) => {
     .catch(err => res.status(400).json('unable to get entries'))
 })
 
-app.listen(443, () => {
-    console.log('app is running on port 3000');
+app.listen(port, () => {
+    console.log('app is running on port ');
 })
 
