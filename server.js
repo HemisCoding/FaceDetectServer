d27307a2dd19c342618d6efdc6ac20e77b6e2788
HemisCoding <plushytowel@gmail.com>
2023-05-17 21:23:04 +0300

    test6

--- server.js
+++ server.js
@@ -115,7 +115,7 @@ app.put('/image', (req, res) => {
     .catch(err => res.status(400).json('unable to get entries'))
 })
 
-app.listen(port, () => {
+app.listen(443, () => {
     console.log('app is running on port ');
 })
 
